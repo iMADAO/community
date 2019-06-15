aa1a03e11d97d507534826603201cecf931a727e
Madao <13420110105@163.com>
2019-06-15 12:48:44 +0800

    a

--- common/src/main/java/com/madao/common/form/UserLoginForm.java
+++
@@ -1,9 +0,0 @@
-package com.madao.common.form;
-
-import lombok.Data;
-
-@Data
-public class UserLoginForm {
-    private String id;
-    private String password;
-}
--- common/src/main/java/com/madao/common/utils/ResultView.java
+++
@@ -1,9 +0,0 @@
-package com.madao.common.utils;
-import lombok.Data;
-
-@Data
-public class ResultView {
-    private Byte code;
-    private String hint;
-    private Object data;
-}
